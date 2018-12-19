5786492fccfd4c761eed020ec11c04106f9ed635
ritishkarki <karki.ritish@gmail.com>
2018-12-19 20:32:40 +0545

    fixed header

--- header.php
+++ header.php
@@ -22,41 +22,41 @@
 		<div class="row">
 			<div class="col-md-12">
 
-				<header id="masthead" class="site-header group" role="banner" itemscope="itemscope" itemtype="http://schema.org/Organization">
+				<header id="masthead" class="site-header group fixed" role="banner" itemscope="itemscope" itemtype="http://schema.org/Organization">
+					<div class="container">
+						<div class="site-logo">
+							<!-- <h1 itemprop="name">
+								<a itemprop="url" href="<?php echo esc_url( home_url() ); ?>">
+									<?php if ( get_theme_mod( 'logo', get_template_directory_uri() . '/images/logo.png' ) ): ?>
+										<img itemprop="logo"
+												src="<?php echo esc_url( get_theme_mod( 'logo', get_template_directory_uri() . '/images/logo.png' ) ); ?>"
+												alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"/>
+									<?php else: ?>
+										<?php //bloginfo( 'name' ); ?>
+									<?php endif; ?>
+								</a>
+							</h1> -->
 
-					<div class="site-logo">
-						<!-- <h1 itemprop="name">
-							<a itemprop="url" href="<?php echo esc_url( home_url() ); ?>">
-								<?php if ( get_theme_mod( 'logo', get_template_directory_uri() . '/images/logo.png' ) ): ?>
-									<img itemprop="logo"
-									     src="<?php echo esc_url( get_theme_mod( 'logo', get_template_directory_uri() . '/images/logo.png' ) ); ?>"
-									     alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"/>
-								<?php else: ?>
-									<?php //bloginfo( 'name' ); ?>
-								<?php endif; ?>
-							</a>
-						</h1> -->
+							<?php if ( get_bloginfo( 'description' ) ): ?>
+								<h2 class="tagline"><?php bloginfo( 'description' ); ?></h2>
+							<?php endif; ?>
+							<?php include 'searchform.php';?>
+						</div><!-- /site-logo -->
 
-						<?php if ( get_bloginfo( 'description' ) ): ?>
-							<h2 class="tagline"><?php bloginfo( 'description' ); ?></h2>
-						<?php endif; ?>
-						<?php include 'searchform.php';?>
-					</div><!-- /site-logo -->
+						<div class="site-bar group">
+							<nav class="nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
+								<?php wp_nav_menu( array(
+									'theme_location' => 'main_menu',
+									'container'      => '',
+									'menu_id'        => '',
+									'menu_class'     => 'navigation'
+								) ); ?>
 
-					<div class="site-bar group">
-						<nav class="nav" role="navigation" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement">
-							<?php wp_nav_menu( array(
-								'theme_location' => 'main_menu',
-								'container'      => '',
-								'menu_id'        => '',
-								'menu_class'     => 'navigation'
-							) ); ?>
-
-							<a class="mobile-nav-trigger" href="#mobilemenu"><i class="fa fa-navicon"></i> <?php esc_html_e( 'Menu', 'olsen-light' ); ?></a>
-						</nav>
-						<div id="mobilemenu"></div>
-					</div><!-- /site-bar -->
-
+								<a class="mobile-nav-trigger" href="#mobilemenu"><i class="fa fa-navicon"></i> <?php esc_html_e( 'Menu', 'olsen-light' ); ?></a>
+							</nav>
+							<div id="mobilemenu"></div>
+						</div><!-- /site-bar -->
+					<div>
 				</header>
 
 				<?php if ( is_home() ) {
